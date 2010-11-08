36351d0fdbab5455b832bd3c99efc5a4b550de33
Masaki Fujimoto <fujimoto@php.net>
2010-11-08 23:44:38 +0900

    added test for key_resolver_modular

---
+++ test/lib/test-key-resolver-modular.cc
@@ -0,0 +1,52 @@
+/**
+ *	test-key-resolver-modular.cc
+ *
+ *	@author	Masaki Fujimoto <fujimoto@php.net>
+ */
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <cppcutter.h>
+
+#include <cluster.h>
+#include <key_resolver_modular.h>
+#include <storage.h>
+
+using namespace std;
+using namespace gree::flare;
+
+namespace test_key_resolver_modular {
+	key_resolver*		kr;
+
+	void setup() {
+		kr = new key_resolver_modular(1);
+		kr->startup();
+	}
+
+	void teardown() {
+		delete kr;
+	}
+
+	void test_partition() {
+		storage::entry e;
+		int r;
+
+		int test_key1_assert[] = {0, 0, 2, 2, 2, 5, 5, 5};
+		for (int i = 0; i < sizeof(test_key1_assert) / sizeof(int); i++) {
+			r = kr->resolve(e.get_key_hash_value("test_key1"), i+1);
+			cppcut_assert_equal(test_key1_assert[i], r);
+		}
+
+		int test_key2_assert[] = {0, 0, 0, 0, 0, 5, 5, 5};
+		for (int i = 0; i < sizeof(test_key2_assert) / sizeof(int); i++) {
+			r = kr->resolve(e.get_key_hash_value("::test::key::1979"), i+1);
+			cppcut_assert_equal(test_key2_assert[i], r);
+		}
+
+		int test_key3_assert[] = {0, 0, 2, 2, 2, 2, 2, 2};
+		for (int i = 0; i < sizeof(test_key3_assert) / sizeof(int); i++) {
+			r = kr->resolve(e.get_key_hash_value("::test::key::19790217"), i+1);
+			cppcut_assert_equal(test_key3_assert[i], r);
+		}
+	}
+}

269a3bae2b04c7a6b5c3394825b7d866216c2bb8
SEJIMA Takanori <takanori.sejima@gmail.com>
2011-05-27 16:47:20 +0900

    fix 'show variables'
    fix format

--- src/flarei/op_show_index.cc
+++ src/flarei/op_show_index.cc
@@ -58,24 +58,24 @@ int op_show_index::_run_server() {
 int op_show_index::_send_show_variables() {
 	ostringstream s;
 
-	s << "config_path               " << ini_option_object().get_config_path().c_str() << line_delimiter;
-	s << "pid_path                  " << ini_option_object().get_pid_path().c_str() << line_delimiter;
-	s << "daemonize                 " << (ini_option_object().is_daemonize() ? "true" : "false") << line_delimiter;
-	s << "data_dir                  " << ini_option_object().get_data_dir().c_str() << line_delimiter;
-	s << "max_connection            " << ini_option_object().get_max_connection() << line_delimiter;
-	s << "monitor_threshold         " << ini_option_object().get_monitor_threshold() << line_delimiter;
-	s << "monitor_interval          " << ini_option_object().get_monitor_interval() << line_delimiter;
-	s << "monitor_read_timeout      " << ini_option_object().get_monitor_read_timeout() << line_delimiter;
-	s << "net_read_timeout          " << ini_option_object().get_net_read_timeout() << line_delimiter;
-	s << "partition_modular_hint    " << ini_option_object().get_partition_modular_hint() << line_delimiter;
-	s << "partition_modular_virtual " << ini_option_object().get_partition_modular_virtual() << line_delimiter;
-	s << "partition_size            " << ini_option_object().get_partition_size() << line_delimiter;
-	s << "partition_type            " << ini_option_object().get_partition_type().c_str() << line_delimiter;
-	s << "server_name               " << ini_option_object().get_server_name().c_str() << line_delimiter;
-	s << "server_port               " << ini_option_object().get_server_port() << line_delimiter;
-	s << "server_socket             " << ini_option_object().get_server_socket().c_str() << line_delimiter;
-	s << "stack_size                " << ini_option_object().get_stack_size() << line_delimiter;
-	s << "thread_pool_size          " << ini_option_object().get_thread_pool_size() << line_delimiter;
+	s << "config_path:              " << ini_option_object().get_config_path().c_str() << line_delimiter;
+	s << "pid_path:                 " << ini_option_object().get_pid_path().c_str() << line_delimiter;
+	s << "daemonize:                " << (ini_option_object().is_daemonize() ? "true" : "false") << line_delimiter;
+	s << "data_dir:                 " << ini_option_object().get_data_dir().c_str() << line_delimiter;
+	s << "max_connection:           " << ini_option_object().get_max_connection() << line_delimiter;
+	s << "monitor_threshold:        " << ini_option_object().get_monitor_threshold() << line_delimiter;
+	s << "monitor_interval:         " << ini_option_object().get_monitor_interval() << line_delimiter;
+	s << "monitor_read_timeout:     " << ini_option_object().get_monitor_read_timeout() << line_delimiter;
+	s << "net_read_timeout:         " << ini_option_object().get_net_read_timeout() << line_delimiter;
+	s << "partition_modular_hint:   " << ini_option_object().get_partition_modular_hint() << line_delimiter;
+	s << "partition_modular_virtual:" << ini_option_object().get_partition_modular_virtual() << line_delimiter;
+	s << "partition_size:           " << ini_option_object().get_partition_size() << line_delimiter;
+	s << "partition_type:           " << ini_option_object().get_partition_type().c_str() << line_delimiter;
+	s << "server_name:              " << ini_option_object().get_server_name().c_str() << line_delimiter;
+	s << "server_port:              " << ini_option_object().get_server_port() << line_delimiter;
+	s << "server_socket:            " << ini_option_object().get_server_socket().c_str() << line_delimiter;
+	s << "stack_size:               " << ini_option_object().get_stack_size() << line_delimiter;
+	s << "thread_pool_size:         " << ini_option_object().get_thread_pool_size();
 
 	this->_connection->writeline(s.str().c_str());
 
